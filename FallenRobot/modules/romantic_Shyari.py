e52d3c32756744635dd09833178a8ff2d054a6db
cyberxron <99018732+cyberxron@users.noreply.github.com>
2022-06-21 14:08:37 +0530

    Update romantic_Shyari.py

--- FallenRobot/modules/romantic_Shyari.py
+++ FallenRobot/modules/romantic_Shyari.py
@@ -34,7 +34,7 @@ ROMANTIC_STRINGS = [
     Coded by : @CoderX on telegram...
 """
 
-@scenario.on_message(filters.command("romantic"))
+@FallenRobot.on_message(filters.command("shayari"))
 async def lel(bot, message):
     ran = random.choice(ROMANTIC_STRINGS)
     await bot.send_chat_action(message.chat.id, "typing")
