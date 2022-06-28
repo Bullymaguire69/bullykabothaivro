9bfb8f354a5cf04ef76cce38c7fb90eaf9e60c0e
cyberxron <99018732+cyberxron@users.noreply.github.com>
2022-06-28 22:58:33 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -3,7 +3,7 @@ import time
 import re
 from sys import argv
 from typing import Optional
-import scenario.modules.sql.users_sql as sql
+import FallenRobot.modules.sql.users_sql as sql
 
 from FallenRobot import (
     ALLOW_EXCL,
